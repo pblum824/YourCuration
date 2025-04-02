b6ef45c1355404c64f4bf494fa31058e70a14bcf
Philip Blum <40996637-q4xj5vzccd@users.noreply.replit.com>
2025-04-02 12:45:55 +0000

    Add error overlay to debug blank screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import './PhotoSorter.css';
-import './ArtistDashboard.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+try {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<App />);
+} catch (err) {
+  document.body.innerHTML = `
+    <pre style="padding: 2rem; font-size: 1.1rem; color: red; background: #fff;">
+      ${err.message}
+    </pre>
+  `;
+}
