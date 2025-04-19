15665ed3c8351922b67ef71318ab70f9bb4365e1
Philip Blum <40996637-q4xj5vzccd@users.noreply.replit.com>
2025-04-19 01:18:32 +0000

    simplified cliptext.js

--- src/utils/clipText.js
+++ src/utils/clipText.js
@@ -1,31 +1,17 @@
 // utils/clipText.js
 import * as ort from 'onnxruntime-web';
 
-let textModelSession = null;
-
 export async function loadTextModelSession() {
-  if (textModelSession) return textModelSession;
-  textModelSession = await ort.InferenceSession.create(
-    'https://yourcuration-static.s3.us-east-2.amazonaws.com/models/clip-text-vit-b32.onnx'
-  );
-  return textModelSession;
+  const modelPath = 'https://yourcuration-static.s3.us-east-2.amazonaws.com/models/clip-text-vit-b32.onnx';
+  return await ort.InferenceSession.create(modelPath);
 }
 
 export async function getTextFeatures(prompts, session) {
   try {
     console.log('[Tokenizer] Running ONNX inference with prompts...');
-
     const tensor = new ort.Tensor('string', prompts, [prompts.length]);
-    const feeds = { text: tensor };
-
-    const output = await session.run(feeds);
-    const embeddings = output['text_features'].data;
-    const dim = output['text_features'].dims[1];
-    const reshaped = [];
-    for (let i = 0; i < embeddings.length; i += dim) {
-      reshaped.push(embeddings.slice(i, i + dim));
-    }
-    return reshaped;
+    const output = await session.run({ input: tensor });
+    return output.text_features.data;
   } catch (err) {
     console.error('[Tokenizer] FAILED during getTextFeatures:', err);
     return [];
