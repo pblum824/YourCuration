f9f2d8f76453d1c3d7e119110f8b93a83072a78a
Philip Blum <40996637-q4xj5vzccd@users.noreply.replit.com>
2025-05-17 15:22:45 +0000

    add log after tags received

--- src/GenerateTags.jsx
+++ src/GenerateTags.jsx
@@ -38,6 +38,7 @@ export default function GenerateTags() {
             if (!res.ok) throw new Error(`Failed (${res.status})`);
 
             const result = await res.json();
+            console.log('Result:', result);
             const imageTags = result.imageTags || [];
             const textTags = result.textTags || [];
             const frontendTags = result.frontendTags || [];
